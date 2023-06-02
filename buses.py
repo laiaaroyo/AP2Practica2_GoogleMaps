6ffb69a00b35a7c729d89d3a2b6f5a2ea7f920a8
laiaaroyo <113499732+laiaaroyo@users.noreply.github.com>
2023-06-02 12:35:22 +0200

    Update buses.py

--- buses.py
+++ buses.py
@@ -1,54 +1,36 @@
 import networkx as nx
 import matplotlib.pyplot as plt
-import staticmaps
+import staticmap
 from typing import TypeAlias
-from dataclasses import dataclass
 import json
 import urllib.request
 import numpy as np
 import pandas as pd
+import math
 
 BusesGraph : TypeAlias = nx.Graph
 Coord : TypeAlias = tuple[float, float]
 
-@dataclass
-class Parada:
-    nom: str
-    linies: list[str]
-    pos: Coord
-
-    def __init__(self, nom, linia: str, pos: Coord) -> None:
-        self.nom = nom
-        self.linies = [linia]
-        self.pos = pos
-
-    def add_a_line(self, linia: str) -> None:
-        self.linies.append(linia)
-
-class Bus:
-    v_by_bus: int = 15 #km/h
-    dist: float 
-    t_espera: int = 0
-    duration: float
-
-    def __init__(self, dist: float) -> None:
-        self.dist = dist
-        self.duration = self.dist*self.v_by_bus
-
-    def revalue(self, newdist: float) -> float:
-        '''Recalcula la duracio quan actualitzem la distancia'''
-        self.dist = newdist
-        self.duration = self.dist*self.v_by_bus
-    
-    def calculate_duration(self, wait: bool) -> float:
-        '''Calcula el que tardara en anar en bus segons si ja estava pujat al bus o l'ha d'esperar'''
-        if wait:
-            return self.duration + self.t_espera
-        return self.duration
-
 def distance(p1: Coord, p2: Coord) -> float:
     '''Donades dies coordenades calcula la distancia entre elles a vol d'ocell'''
-    return np.sqrt((p1[0]-p2[0])**2+(p1[1]-p2[1])**2)
+    radi = 6371.0
+
+    #Convertim les coordenades de graus a radians
+    lat1_rad = math.radians(p1[0])
+    lon1_rad = math.radians(p1[1])
+    lat2_rad = math.radians(p2[0])
+    lon2_rad = math.radians(p2[1])
+
+    #Diferències entre latitud i longitud
+    dlat = lat2_rad - lat1_rad
+    dlon = lon2_rad - lon1_rad
+
+    #Utilitzem la fòrmula de Haversine
+    a = math.sin(dlat / 2) ** 2 + math.cos(lat1_rad) * math.cos(lat2_rad) * math.sin(dlon / 2) ** 2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+
+    #Calculem la distància i la retornem en metres
+    return radi * c * 1000
 
 def get_buses_graph() -> BusesGraph:
     """Retorna un graf a partir de les dades dels busos de l'AMB."""
@@ -56,22 +38,24 @@ def get_buses_graph() -> BusesGraph:
     busos = json.load(dades_busos_json)
     graf: BusesGraph = nx.Graph()
     first = True
-
+    
     for linia in busos['ObtenirDadesAMBResult']['Linies']['Linia']:
         for parada in linia['Parades']['Parada']:
-            if parada['CodAMB'] in dict(graf.nodes):
-                dict(graf.nodes)[parada['CodAMB']]['info'].add_a_line(parada['IdLinia'])
-            else:
-                graf.add_node(parada['CodAMB'], info=Parada(parada['Nom'], parada['IdLinia'], pos=[-parada['UTM_X'], -parada['UTM_Y']]), pos=[-parada['UTM_X'], -parada['UTM_Y']])
-                    
-            if first:
-                first = False
+            #Agafem nomes les parades de Barcelona
+            if parada['Municipi'] == 'Barcelona':
+                if parada['CodAMB'] in dict(graf.nodes):
+                    dict(graf.nodes)[parada['CodAMB']]['linia'].append(parada['IdLinia'])
+                else:
+                    graf.add_node(parada['CodAMB'], nom=parada['Nom'], linia=[parada['IdLinia']], pos=[parada['UTM_X'], parada['UTM_Y']])
+                                                    
+                if first:
+                    first = False
 
-            elif linia == parada['IdLinia'] and parada['CodAMB'] != parada_antiga:
-                graf.add_edge(parada['CodAMB'], parada_antiga, info=Bus(distance(dict(graf.nodes)[parada['CodAMB']]['pos'], dict(graf.nodes)[parada_antiga]['pos'])))
+                elif linia == parada['IdLinia'] and parada['CodAMB'] != parada_antiga:
+                    graf.add_edge(parada['CodAMB'], parada_antiga, dist=distance(dict(graf.nodes)[parada['CodAMB']]['pos'], dict(graf.nodes)[parada_antiga]['pos']), pos1=dict(graf.nodes)[parada['CodAMB']]['pos'], pos2=dict(graf.nodes)[parada_antiga]['pos'])
 
-            linia = parada['IdLinia']
-            parada_antiga = parada['CodAMB']
+                linia = parada['IdLinia']
+                parada_antiga = parada['CodAMB']
 
     return graf
 
@@ -81,32 +65,21 @@ def show(g: BusesGraph) -> None:
     nx.draw(g, node_size=0, pos=pos)
     plt.show()
 
-    'o sino'
-    '''
-    mapa = folium.Map(location=[2.1589900, 41.3887900])
-    for p in g.nodes:
-        folium.Marker(p['info'].pos, popup = p['info'].nom, tooltip = p['info'].nom).add_to(mapa)
-    '''
-
-   def plot(g: BusesGraph, nom_fitxer: str) -> None:
+def plot(g: BusesGraph, nom_fitxer: str) -> None:
     """Desa el graf com una imatge amb el mapa de la ciutat de fons en l'arxiu especificat."""
     
     mapa = staticmap.StaticMap(800, 800)
     
-    for node in g.nodes:
-        p = (node['info']['pos'][0], node['info']['pos'][1])
-        mapa.add_marker(staticmap.CircleMarker(p, 'red', 5))
+    for node in dict(g.nodes).values():
+        p = (node['pos'][1], node['pos'][0])
+        mapa.add_marker(staticmap.CircleMarker(p, 'red', 1))
 
-    for edge in g.edges:
-        s = (edge.source['info']['pos'][0], edge.source['info']['pos'][1])
-        d = (edge.target['info']['pos'][0], edge.target['info']['pos'][1])
-        mapa.add_line(staticmap.Line([s, d], 'blue', 1))
+    for edge in dict(g.edges).values():
+        if edge['dist'] < 10000: #si la distancia de l'aresta és de més de 10km, no la volem mostrar ja que tan sols representa les unions entre els dos extrems de linia
+            s = edge['pos2'][1], edge['pos2'][0]
+            d = edge['pos1'][1], edge['pos1'][0]
+            mapa.add_line(staticmap.Line([s, d], 'blue', 1))
 
     #Guardem la imatge en un fitxer
     imatge = mapa.render()
     imatge.save(nom_fitxer)
-'''
-def main():
-    graf = get_buses_graph()
-    show(graf)
-'''
