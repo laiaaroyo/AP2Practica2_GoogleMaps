63be849ad6838f500ea7afbb7b06f11fbd934e8c
laiaaroyo <113499732+laiaaroyo@users.noreply.github.com>
2023-06-04 13:57:45 +0200

    Update billboard.py

--- billboard.py
+++ billboard.py
@@ -5,9 +5,6 @@ import requests
 from requests.exceptions import RequestException
 import json
 import time
-import datetime
-import yogi
-
 
 @dataclass 
 class Film: 
@@ -15,7 +12,6 @@ class Film:
     genre: str
     director: str
     actors: list[str]
-
 
 @dataclass 
 class Cinema: 
@@ -26,14 +22,13 @@ class Cinema:
 class Projection: 
     film: Film
     cinema: Cinema
-    time: tuple[int, int]   # hora:minut
-    
+    time: tuple[int, int] # hora:minut
 
 @dataclass 
 class Billboard: 
-    films: list[Film] #pelis disponibles
-    cinemas: list[Cinema] #sales de cine disponible
-    projections: list[Projection] #pelicules en la seva corresponent sala, juntament amb la seva hora de projecció.
+    films: list[Film] # Pelis disponibles
+    cinemas: list[Cinema] # Sales de cine disponibles
+    projections: list[Projection] # Pellícules en la seva corresponent sala, juntament amb la seva hora de projecció.
     
     def cerca_peli_per_nom(self,keyword:str) -> list[Projection]:
         """Donat un nom d'una pel·lícula ens retorna una llista de projeccions amb els cinemes
@@ -46,7 +41,6 @@ class Billboard:
         """Seleccionem les pel·lícules del gènere que ens interessa. Retornem una llista de projeccions"""
         
         return [projection for projection in self.projections if genre in projection.film.genre]
-
 
     def cerca_peli_per_director(self,director:str) -> list[Projection]:
         """Seleccionem les pel·lícules del director que ens interessa. Retornem una llista de projeccions"""
@@ -90,20 +84,17 @@ def read() -> Billboard:
             return Billboard(list(films.values()), cinemes, projections)
 
 
-
-
 def read_list_cinemas(contingut:BeautifulSoup, cinemes:dict[str,str] ) -> dict[str,str]:
     """Llegim totes les sales de cinema que hi ha a Barcelona i fem un diccionari, 
     associant cada cinema amb la seva direcció."""
  
-    #llegim tots els cinemes
+    # Llegim tots els cinemes
     sales_cinema : list[str] = []
     sales = contingut.find_all('a', class_='no_underline j_entities')
     for sala in sales:
         sales_cinema.append(sala.text.replace('\n', '').strip()) 
-        
     
-    #llegim totes les adresses dels cinemes
+    # Llegim totes les adresses dels cinemes
     adresses = contingut.select('span.lighten:not(.fr.fs11.lighten)')
     idx = 0
     for direccio in adresses:
@@ -117,7 +108,7 @@ def read_list_films(contingut:BeautifulSoup,films:dict[str,Film]) -> dict[str,Fi
     """Llegim totes les pel·lícules disponibles del dia actual, i fem un diccionari, associant
     el nom de cada pel·lícula amb les característiques principals de la pel·lícula."""
 
-    #Seleccionem només les pel·lícules del dia actual
+    # Seleccionem només les pel·lícules del dia actual
     dia_actual = contingut.find_all('div',class_ = 'tabs_box_pan item-0')
     
     for pelicula_actual in dia_actual:
@@ -125,7 +116,7 @@ def read_list_films(contingut:BeautifulSoup,films:dict[str,Film]) -> dict[str,Fi
         for entry in data: 
             divs_w = entry.find_all('div',class_ = 'j_w') 
             data_movie = divs_w[0]['data-movie']
-            data_movie = json.loads(data_movie) #convertim en un diccionari
+            data_movie = json.loads(data_movie) # Ho convertim en un diccionari
             title = data_movie['title']
             directors = data_movie['directors']
             actors = data_movie['actors']
@@ -133,7 +124,7 @@ def read_list_films(contingut:BeautifulSoup,films:dict[str,Film]) -> dict[str,Fi
 
             new_film = Film(title, genre, directors, actors)
 
-            #Ens assegurem de no tenir pel·lícules repetides.   
+            # Ens assegurem de no tenir pel·lícules repetides.   
             if new_film not in films.values():
                 films[title] = new_film
 
@@ -144,7 +135,7 @@ def read_list_projections(contingut: BeautifulSoup,cinemes:dict[str,str],films:d
     """Llegim les hores en què podem visualitzar cada pel·lícula i retornem una llista de projeccions,
     on cada projecció consisteix en la pel·lícula, el cinema en la que la podem veure i l'hora."""
 
-    #Seleccionem només les pel·lícules que estiguin programades per projectar-se el dia actual.
+    # Seleccionem només les pel·lícules que estiguin programades per projectar-se el dia actual.
     cines = contingut.find_all('div',class_= 'tabs_box_pan item-0')
 
     for cine in cines:
@@ -154,29 +145,26 @@ def read_list_projections(contingut: BeautifulSoup,cinemes:dict[str,str],films:d
             data_movie = div_entry['data-movie']
             data_movie_json = json.loads(data_movie)
             title = data_movie_json['title']
-        
 
-            #Seleccionem la pel·lícula segons el títol recuperat
-            pelicula_actual = films[title]
+            # Seleccionem la pel·lícula segons el títol recuperat
+            pelicula_actual = films[title]  
                 
-                
-            #Seleccionem el cinema on es pot veure la pel·lícula en funció del nom recuperat
+            # Seleccionem el cinema on es pot veure la pel·lícula en funció del nom recuperat
             data_theater = div_entry['data-theater']
             data_theater = json.loads(data_theater)
             name = data_theater['name'].strip()
             cinema_actual = Cinema(name,cinemes[name])
-
         
-            #busquem els horaris disponibles per la pel·lícula
+            # Busquem els horaris disponibles per la pel·lícula
             u_items = projeccio.find_all('ul',class_='list_hours')
 
             for u_item in u_items:
                 em_tags = u_item.find_all('em')
     
-                #Iterem sobre els horaris extrets
+                # Iterem sobre els horaris extrets
                 for em_tag in em_tags:
                     
-                    #convertim la cadena en una llista i agafem l'hora d'inici.
+                    # Convertim la cadena en una llista i agafem l'hora d'inici.
                     data_times = em_tag['data-times']
                     data_times = json.loads(data_times)
                     hora_inici = data_times[0]
@@ -184,6 +172,5 @@ def read_list_projections(contingut: BeautifulSoup,cinemes:dict[str,str],films:d
                     time = (int(hora), int(minuts))
    
                     projections.append(Projection(pelicula_actual,cinema_actual,time))
-            
-    
+          
     return projections
