3ed446641fe7691d8d47bd0f08581b05acf8c327
laiaaroyo <113499732+laiaaroyo@users.noreply.github.com>
2023-06-04 13:58:52 +0200

    Update buses.py

--- buses.py
+++ buses.py
@@ -15,22 +15,20 @@ def distance(p1: Coord, p2: Coord) -> float:
     '''Donades dies coordenades calcula la distancia entre elles a vol d'ocell'''
     radi = 6371.0
 
-    #Convertim les coordenades de graus a radians
+    # Convertim les coordenades de graus a radians
     lat1_rad = math.radians(p1[0])
     lon1_rad = math.radians(p1[1])
     lat2_rad = math.radians(p2[0])
     lon2_rad = math.radians(p2[1])
 
-    #Diferències entre latitud i longitud
     dlat = lat2_rad - lat1_rad
     dlon = lon2_rad - lon1_rad
 
-    #Utilitzem la fòrmula de Haversine
+    # Utilitzem la fòrmula de Haversine
     a = math.sin(dlat / 2) ** 2 + math.cos(lat1_rad) * math.cos(lat2_rad) * math.sin(dlon / 2) ** 2
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
 
-    #Calculem la distància i la retornem en metres
-    return radi * c * 1000
+    return radi * c * 1000 #meters
 
 def get_buses_graph() -> BusesGraph:
     """Retorna un graf a partir de les dades dels busos de l'AMB."""
@@ -41,7 +39,8 @@ def get_buses_graph() -> BusesGraph:
     
     for linia in busos['ObtenirDadesAMBResult']['Linies']['Linia']:
         for parada in linia['Parades']['Parada']:
-            #Agafem nomes les parades de Barcelona
+            # Agafem nomes les parades de Barcelona
+
             if parada['Municipi'] == 'Barcelona':
                 if parada['CodAMB'] in dict(graf.nodes):
                     dict(graf.nodes)[parada['CodAMB']]['linia'].append(parada['IdLinia'])
@@ -52,7 +51,7 @@ def get_buses_graph() -> BusesGraph:
                     first = False
 
                 elif linia == parada['IdLinia'] and parada['CodAMB'] != parada_antiga:
-                    graf.add_edge(parada['CodAMB'], parada_antiga, dist=distance(dict(graf.nodes)[parada['CodAMB']]['pos'], dict(graf.nodes)[parada_antiga]['pos']), pos1=dict(graf.nodes)[parada['CodAMB']]['pos'], pos2=dict(graf.nodes)[parada_antiga]['pos'])
+                    graf.add_edge(parada['CodAMB'], parada_antiga, dist=0, pos1=dict(graf.nodes)[parada['CodAMB']]['pos'], pos2=dict(graf.nodes)[parada_antiga]['pos'])
 
                 linia = parada['IdLinia']
                 parada_antiga = parada['CodAMB']
@@ -75,11 +74,12 @@ def plot(g: BusesGraph, nom_fitxer: str) -> None:
         mapa.add_marker(staticmap.CircleMarker(p, 'red', 1))
 
     for edge in dict(g.edges).values():
-        if edge['dist'] < 10000: #si la distancia de l'aresta és de més de 10km, no la volem mostrar ja que tan sols representa les unions entre els dos extrems de linia
+        # Si la distancia de l'aresta és de més de 10km, no la volem mostrar ja que tan sols representa les unions entre els dos extrems de linia
+        if edge['dist'] < 10000: 
             s = edge['pos2'][1], edge['pos2'][0]
             d = edge['pos1'][1], edge['pos1'][0]
             mapa.add_line(staticmap.Line([s, d], 'blue', 1))
 
-    #Guardem la imatge en un fitxer
+    # Guardem la imatge en un fitxer
     imatge = mapa.render()
     imatge.save(nom_fitxer)
